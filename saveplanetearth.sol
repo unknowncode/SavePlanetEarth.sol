d3e34b07ffcbf25ea5b37060d345c5ff5435259f
Aseef Khan <aseef@boilerplate.in>
2021-06-15 09:23:00 +0530

    Removed redundant declaration of variable MAX

--- saveplanetearth.sol
+++ saveplanetearth.sol
@@ -695,9 +695,8 @@ contract SavePlanetEarth is Context, IERC20, Ownable {
     mapping (address => bool) private _isExcluded;
     address[] private _excluded;
    
-    uint256 private constant MAX = ~uint256(0);
     uint256 private constant _tTotal = 1000000000 * 10**6 * 10**9;
-    uint256 private _rTotal = (MAX - (MAX % _tTotal));
+    uint256 private _rTotal = (~uint256(0) - (~uint256(0) % _tTotal));
     uint256 private _tFeeTotal;
 
     string private constant _name = "SavePlanetEarth";
